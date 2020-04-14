7a65811f481d213be5bdae8d871e115c116b32ce
M. Nakashima <57747800+Calistic@users.noreply.github.com>
2020-04-13 23:46:56 -0700

    Update config.py
    make password default

--- Code/config.py
+++ Code/config.py
@@ -1,3 +1,3 @@
 awsS3_key= 'f7RKyb3MAYsWXbJXb4Hw6MqtRs3/xK76L1kbsKlw'
-db_password = 'SivotalPys'   # DB password
-postgrespass = 'SivotalPys' # HA pass
+db_password = '<password>'   # DB password
+postgrespass = '<password>' # HA pass
