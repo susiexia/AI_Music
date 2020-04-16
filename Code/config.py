5bd7c30096a7a1d9a51126a8cdd502bcf2d6a15a
susiexia <57109686+susiexia@users.noreply.github.com>
2020-04-15 23:14:43 -0700

    add susie's config info

--- Code/config.py
+++ Code/config.py
@@ -1,3 +1,4 @@
 awsS3_key= 'f7RKyb3MAYsWXbJXb4Hw6MqtRs3/xK76L1kbsKlw'
 db_password = 'SivotalPys'   # DB password
 postgrespass = '<password>' # HA pass
+db_password_susie = 1111    # susie password
