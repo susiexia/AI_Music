da02180df0a2c1c8652b54bf4525c631a0142caa
susiexia <57109686+susiexia@users.noreply.github.com>
2020-04-26 20:08:42 -0700

    add francis password

--- Code/config.py
+++ Code/config.py
@@ -1,4 +1,5 @@
 awsS3_key= 'f7RKyb3MAYsWXbJXb4Hw6MqtRs3/xK76L1kbsKlw'
 db_password = 'SivotalPys'   # DB password
 postgrespass = '<password>' # HA pass
-db_password_susie = 1111    # susie password
+db_password_susie = '1111'    # susie password
+db_password_Francis = 'Strawberry1'''     # Francis password
